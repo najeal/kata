735e2a771dc1d377acdb5611b36c6fb8e42c8dd8
nathanh <nathan.haim@free.fr>
2019-04-09 20:44:25 +0200

    Kata02: Karate Chop
    This implements chop function with iterative approach

--- karate_chop.go
+++ karate_chop.go
@@ -2,5 +2,17 @@ package kata
 
 // chop searches the target in the sorted array source
 func chop(target int, source []int) int {
+	min := 0
+	max := len(source) - 1
+	for min <= max {
+		mid := min + (max-min)/2
+		if source[mid] == target {
+			return mid
+		} else if source[mid] > target {
+			max = mid - 1
+		} else {
+			min = mid + 1
+		}
+	}
 	return -1
 }
